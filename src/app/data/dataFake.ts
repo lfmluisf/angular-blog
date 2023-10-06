b2b9eaaf446873b29b8cd82493d0bb2c62d84d95
Luis Fernando <lfm.1383@outlook.com>
2023-10-06 19:07:51 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo Guardiões da Galaxia 3",
+    "description": "Marvel studios lança novo filme dos Guardiões da Galaxia em breve nos cinemas",
+    "photoCover":"https://i0.wp.com/www.arrobanerd.com.br/wp-content/uploads/2023/04/quando-comeca-a-pre-venda-de-ingressos-guardioes-da-galaxia-vol.-3.jpg?fit=1280%2C720&ssl=1"
+  },
+  {
+    "id": "2",
+    "title": "Nova Série Guardiões da Galaxia anunciada no Disney +",
+    "description": "Siga informado",
+    "photoCover":"https://2.bp.blogspot.com/-QIKOqwlGHpc/VWlLPIuceOI/AAAAAAAAK9g/th2JVEFfVYA/s1600/xx.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Nova Série Pantera Negra anunciada no Disney +",
+    "description": "Siga informado",
+    "photoCover":"https://i0.wp.com/cuatrobastardos.com/wp-content/uploads/2018/09/avengersbp-card-1202028188-1535837270290.jpg?fit=1024%2C575&ssl=1"
+  },
+  {
+    "id": "4",
+    "title": "Nova Série Homen de Ferro anunciada no Disney +",
+    "description": "Siga informado",
+    "photoCover":"https://universofantastico.files.wordpress.com/2009/03/homem-de-ferro_animacaoa.jpg?w=584"
+  }
+]
